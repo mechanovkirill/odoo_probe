a9a0c829319ea14fa38491a55e3fe3dea6abeb03
mechanovkirill <mechanovkirill@gmail.com>
2023-04-05 12:45:28 +0400

    Added base.user access to estate property model.

--- custom/estate/__manifest__.py
+++ custom/estate/__manifest__.py
@@ -2,6 +2,9 @@
     'name': "Estate",
     'version': '1.0',
     'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv'
+    ],
     'application': True,
     'license': 'LGPL-3',
 }
--- custom/estate/models/estate_property.py
+++ custom/estate/models/estate_property.py
@@ -3,3 +3,22 @@ from odoo import models, fields
 
 class EstateProperty(models.Model):
     _name = "estate.property"
+    _description = "The probe model."
+
+    name = fields.Char(required=True, size=50)
+    description = fields.Text()
+    postcode = fields.Char(size=20)
+    date_availability = fields.Date(string='Availability Date', readonly=True)
+    expected_price = fields.Float(string='Expected Price', required=True)
+    selling_price = fields.Float(string='Selling Price')
+    bedrooms = fields.Integer()
+    living_area = fields.Integer(string='Living Area')
+    facades = fields.Integer(string='Number of Facades')
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer(string='Garden Area')
+    garden_orientation = fields.Selection(
+        string='Garden Orientation',
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
+    )
+

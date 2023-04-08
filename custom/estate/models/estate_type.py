6ddd6f5c983af54edcf21b763695fc7c0eaee8cb
mechanovkirill <mechanovkirill@gmail.com>
2023-04-08 12:20:07 +0400

    Inline view properties added to the Estate type view.

--- custom/estate/models/estate_type.py
+++ custom/estate/models/estate_type.py
@@ -7,6 +7,11 @@ class EstateType(models.Model):
 
     name = fields.Char(size=40, required=True)
 
+    properties_ids = fields.One2many(comodel_name='estate.property', inverse_name="property_type_id")
+    property_name = fields.Char()
+    property_expected_price = fields.Float()
+
+
     _sql_constraints = [
         ('name_uniq', 'unique (name)', 'The name of the estate type must be unique!')
     ]

6fda361eb5c8c9795ea963c2518ee26c552b918f
mechanovkirill <mechanovkirill@gmail.com>
2023-04-09 22:34:07 +0400

    Quantity, name, price added to created invoice via Command.create(dict).

--- custom/estate_account/models/inherited_estate_property.py
+++ custom/estate_account/models/inherited_estate_property.py
@@ -1,18 +1,29 @@
-from typing import Iterable
-from odoo import models, fields
+from typing import Any
+from odoo import models, Command
+from odoo.exceptions import UserError
 
 
 class InheritedEstateProperty(models.Model):
     _inherit = "estate.property"
 
-    def action_sold_estate(self):
+    def action_sold_estate(self: models.Model) -> Any:
         action = super().action_sold_estate()
         partner_id = None
+        price_unit = None
+        estate_name = None
         for record in self:
             partner_id = record.partner_id
+            price_unit = record.selling_price * 0.06 + 100.0
+            estate_name = record.name
+
+        if not price_unit:
+            raise UserError("Selling price can't be zero.")
+
+        code = self.env['account.journal'].search([], order='id desc', limit=1).code
+        code = int(code) + 1
 
         journal = self.env['account.journal'].create({
-            'name': 'dfgsghghghsgh', 'code': '123235gfffffffhd', 'type': 'sale',
+            'name': 'dfdsgdfsgh', 'code': str(code), 'type': 'sale',
         })
         journal_id = journal.id
 
@@ -23,6 +34,13 @@ class InheritedEstateProperty(models.Model):
             'partner_id': partner_id.id,
             'move_type': 'out_invoice',
             'journal_id': journal_id,
+            "invoice_line_ids": [
+                Command.create({
+                    "name": estate_name,
+                    "quantity": 1,
+                    "price_unit": price_unit,
+                })
+            ],
         })
 
         return action

7b2cf1695a2690058c949aff1a01c39c0464ebd6
mechanovkirill <mechanovkirill@gmail.com>
2023-04-09 15:53:27 +0400

    Void module estate_account was created.

---
+++ custom/estate_account/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': "Estate Account",
+    'version': '1.0',
+    'depends': ['base', 'estate', 'account'],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'application': True,
+    'license': 'LGPL-3',
+}

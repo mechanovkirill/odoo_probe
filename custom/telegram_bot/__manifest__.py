8beeddda72ec7b4f86ea2520a102ce0c36d352bf
mechanovkirill <mechanovkirill@gmail.com>
2023-04-10 23:38:03 +0400

    V0.1 bot before deleted.

--- custom/telegram_bot/__manifest__.py
+++ custom/telegram_bot/__manifest__.py
@@ -3,6 +3,9 @@
     'version': '1.0',
     'depends': ['base'],
     'data': [
+        'security/ir.model.access.csv',
+        'views/ir.bot_views.xml',
+        'views/bot_menus.xml',
     ],
     'application': True,
     'license': 'LGPL-3',
